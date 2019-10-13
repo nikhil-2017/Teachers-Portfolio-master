1e8d7cb73312f8f4260984920e6c6d4afdeee512
Nikhil Gaikwad <55644721+nikhil-2017@users.noreply.github.com>
2019-10-14 00:35:48 +0530

    02
    2019-10-14 css/ js/ folder

---
+++ js/demo/chart-Line-demo.js
@@ -0,0 +1,14 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Area Chart Example
+  Morris.Line({
+ element : 'chart',
+ data:[<?php echo $chart_data; ?>],
+ xkey:'year',
+ ykeys:['profit', 'purchase', 'sale'],
+ labels:['Profit', 'Purchase', 'Sale'],
+ hideHover:'auto',
+ stacked:true
+});
